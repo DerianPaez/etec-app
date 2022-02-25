29c2486273deb96b26eade92faddc1a1e7908ad6
James Loor <mendozajames02@hotmail.com>
2022-02-25 15:52:09 -0500

    Add data & setup of colors

---
+++ src/data/values.data.js
@@ -0,0 +1,17 @@
+export const values = [
+  {
+    id: '001',
+    title: 'Transparencia',
+    description: 'Somos una empresa de servicios estratégicos comerciales dedicada a optimizar la calidad de vida de nuestros. Nuestros valores son muy importantes perro hpta'
+  },
+  {
+    id: '002',
+    title: 'Transparencia',
+    description: 'Somos una empresa de servicios estratégicos comerciales dedicada a optimizar la calidad de vida de nuestros. Nuestros valores son muy importantes perro hpta'
+  },
+  {
+    id: '003',
+    title: 'Transparencia',
+    description: 'Somos una empresa de servicios estratégicos comerciales dedicada a optimizar la calidad de vida de nuestros. Nuestros valores son muy importantes perro hpta'
+  },
+]
