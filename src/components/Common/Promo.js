7b2daed1d5bd5bc880184d0ab56c4a9e6dd97288
James Loor <mendozajames02@hotmail.com>
2022-02-25 22:31:16 -0500

    Add styles & data of section Promotion

--- src/components/Common/Promo.js
+++ src/components/Common/Promo.js
@@ -2,12 +2,13 @@
 import React from 'react'
 import styled from 'styled-components'
 import { theme } from '../../theme'
+import Link from './Link'
 
 const PromoStyled = styled.div`
-  height: 130px;
+  height: 180px;
   background-image: ${({ background }) => (background && `url(${background})`)};
-  background-size: cover;
-  background-position: 50% 50%;
+  background-size: 40%;
+  background-position: right;
   background-repeat: no-repeat;
   display: flex;
   flex-direction: column;
@@ -15,20 +16,22 @@ const PromoStyled = styled.div`
   align-items: flex-start;
   padding: 10px;
   border-radius: 2px;
-  color: ${theme.colors.textWithBackground};
-  border: 2px solid ${theme.colors.stroke};
+  color: ${theme.colors.primary};
+  border: 1px solid ${theme.colors.accent};
   @media (min-width: 768px) {
-    height: 160px;
+    padding: 20px;
+    height: 200px;
   }
   @media (min-width: 1024px) {
-    height: 180px;
+    height: 220px;
   }
 `
-const Promo = ({ background, title, text }) => {
+const Promo = ({ id, background, title, text, url }) => {
   return (
-    <PromoStyled background={background}>
+    <PromoStyled id={id} background={background}>
       <h3>{title}</h3>
       <p>{text}</p>
+      <Link to={url}>Ver más</Link>
     </PromoStyled>
   )
 }
