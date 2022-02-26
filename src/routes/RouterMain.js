e443b0add1afb70499c4707aa0718adb9d7ee947
James Loor <mendozajames02@hotmail.com>
2022-02-26 02:05:38 -0500

    Add useLayoutEffect to RouteMain

--- src/routes/RouterMain.js
+++ src/routes/RouterMain.js
@@ -1,24 +1,35 @@
-import React from 'react'
+import React, { useLayoutEffect } from 'react'
 import {
   BrowserRouter,
   Routes,
   Route,
+  useLocation
 } from 'react-router-dom';
 import { Home, About, Products, Contact } from '../pages'
 import Layout from '../components/Common/Layout'
 
+const Wrapper = ({children}) => {
+  const location = useLocation()
+  useLayoutEffect(() => {
+    document.documentElement.scrollTo(0, 0);
+  }, [location.pathname]);
+  return children
+}
+
 const RouterMain = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/nosotros" element={<About />}/>
-          <Route path="/productos" element={<Products />}/>
-          <Route path="/productos" element={<Products />}/>
-          <Route path="/contacto" element={<Contact />} />
-        </Route>
-      </Routes>
+      <Wrapper>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/nosotros" element={<About />}/>
+            <Route path="/productos" element={<Products />}/>
+            <Route path="/productos" element={<Products />}/>
+            <Route path="/contacto" element={<Contact />} />
+          </Route>
+        </Routes>
+      </Wrapper>
     </BrowserRouter>
   )
 }
