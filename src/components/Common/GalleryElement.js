de2fbe33bdd4ff5ef3223e5a8b0f51dc340b7444
James Loor <mendozajames02@hotmail.com>
2022-02-25 23:55:09 -0500

    Add styles to section GalleryProducts

--- src/components/Common/GalleryElement.js
+++ src/components/Common/GalleryElement.js
@@ -1,33 +1,49 @@
 import React from 'react'
 import styled from 'styled-components'
+import { theme } from '../../theme'
 import Button from './Button'
+import { useNavigate } from 'react-router-dom'
 
 const GalleryElementStyled = styled.div`
   display: grid;
   grid-template-columns: 1fr;
   justify-content: center;
-  align-items: center;
-  padding: 15px;
-  background-image: ${({ background }) => (background && `url(${background})`)};
+  padding: 30px 20px;
+  background: linear-gradient(to right, #00000075 , #00000075), ${({ backgroundUrl }) => backgroundUrl && `url(${backgroundUrl})`};
   background-position: center;
   background-size: cover;
-  border-radius: 15px;
-  background-color: blue;
   min-height: 130px;
-  p {
-    margin-bottom: 5px;
+  color: ${theme.colors.secundary};
+  cursor: pointer;
+
+  h3 {
+    font-size: 1em;
+    font-weight: 1000;
+    margin-bottom: 4px;
   }
+
+  p {
+    font-size: .9em;
+  }
+
   @media (min-width: 768px) {
     min-height: 200px;
   }
 `
-const GalleryElement = ({ type, name, background, className }) => {
+const GalleryElement = ({ name, text, background, url, className }) => {
+  let navigate = useNavigate()
+  const handleClick = () => {
+    if (url.startsWith('http') || url.startsWith('//')) {
+      window.open(url)
+    } else {
+      navigate(url)
+    }
+  }
   return (
-    <GalleryElementStyled background={background} className={className}>
+    <GalleryElementStyled onClick={handleClick} backgroundUrl={background} className={className}>
       <div className="element-content">
-        <h3>{type}</h3>
-        <p>{name}</p>
-        <Button type="button">Comprar</Button>
+        <h3>{name}</h3>
+        <p>{text}</p>
       </div>
     </GalleryElementStyled>
   )
